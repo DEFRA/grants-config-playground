c0d2a6b45bb4c0a4a8d0064bef9c064b73c82449
Ben Hope <bendef24@gmail.com>
2026-05-19 16:12:22 +0100

    feat(GRAN-20): basic service setup

--- src/service/config-store-and-inform.js
+++ src/service/config-store-and-inform.js
@@ -1,8 +1,7 @@
-
 export const storePublishedConfigAndInformBroker = async (logger) => {
   logger.info('storing config in s3')
   // store config in s3
 
-  logger.info('informing config broker new version to release')
+  logger.info('informing config broker there is a new version to release')
   // inform config broker new version to release
 }
