ff60a0821335b4adb1b1368928e55d300e65d71a
Ben Hope <bendef24@gmail.com>
2026-06-04 11:06:08 +0100

    feat(GRAN-8): test notifications (#95)

---
+++ .changeset/modern-ravens-deny.md
@@ -0,0 +1,5 @@
+---
+'grants-config-playground': patch
+---
+
+feat(GRAN-8): test notifications
--- src/config.js
+++ src/config.js
@@ -87,59 +87,6 @@ export const config = convict({
       default: 'x-cdp-request-id',
       env: 'TRACING_HEADER'
     }
-  },
-  aws: {
-    endpointUrl: {
-      doc: 'AWS Endpoint URL used for LocalStack',
-      format: String,
-      nullable: true,
-      default: null,
-      env: 'AWS_ENDPOINT_URL'
-    },
-    region: {
-      doc: 'AWS Region',
-      format: String,
-      default: 'eu-west-2',
-      env: 'AWS_REGION'
-    },
-    s3: {
-      bucketName: {
-        doc: 'Configs S3 bucket name',
-        format: String,
-        default: 'configs-bucket',
-        env: 'CONFIG_BUCKET_NAME'
-      },
-      forcePathStyle: {
-        doc: 'Force path style on S3 bucket',
-        format: Boolean,
-        default: true,
-        env: 'FORCE_PATH_STYLE'
-      }
-    }
-  },
-  configBroker: {
-    auth: {
-      token: {
-        doc: 'Bearer token for service-to-service authentication',
-        format: String,
-        default: '',
-        env: 'GRANTS_CONFIG_BROKER_AUTH_TOKEN',
-        sensitive: true
-      },
-      encryptionKey: {
-        doc: 'Encryption key for decrypting bearer token',
-        format: String,
-        default: '',
-        env: 'GRANTS_CONFIG_BROKER_ENCRYPTION_KEY',
-        sensitive: true
-      }
-    },
-    apiEndpoint: {
-      doc: 'Endpoint for the config broker API',
-      format: String,
-      default: 'http://localhost:3001',
-      env: 'GRANTS_CONFIG_BROKER_API_ENDPOINT'
-    }
   }
 })
 
